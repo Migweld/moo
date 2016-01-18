8ad4cdd73eec5e1470e79aabebe3f96540c9924f
Craig Thompson <craig@flodesign.co.uk>
2016-01-18 20:30:23 +0000

    Updated tests

--- test/index.js
+++ test/index.js
@@ -3,14 +3,14 @@ var expect = require('expect');
 
 describe('moo', function () {
 	it('should return a response', function() {
-		
+
 	});
 });
 
 describe('POST / send JSON with token and some text', function() {
 	it('Should respond with a cow saying some text', function(done){
 		var text = 'the cow says Moo';
-		request.post(process.env.URL)
+		request.post('https://frozen-gorge-6050.herokuapp.com/')
 			.type('json')
 			.send({"token": process.env.SLACK_TOKEN, "text": text})
 			.end(function(err,res){
@@ -23,9 +23,9 @@ describe('POST / send JSON with token and some text', function() {
 
 describe('POST / send JSON with token but no text', function() {
 	it('Should return an error', function(done){
-		request.post(process.env.URL)
+		request.post('https://frozen-gorge-6050.herokuapp.com/')
 			.type('json')
-			.send({"token": "xoxp-2520761847-2521679878-18784221010-76381320c8", "text": ''})
+			.send({"token": process.env.SLACK_TOKEN, "text": ''})
 			.end(function(err, res){
 				expect(res.status).toBe(400);
 				expect(res.text).toEqual('You must give the cow something to say!');
@@ -36,7 +36,7 @@ describe('POST / send JSON with token but no text', function() {
 
 describe('Send a GET request', function(){
 	it('Should return an error', function(done){
-		request.get(process.env.URL)
+		request.get('https://frozen-gorge-6050.herokuapp.com/')
 		.end(function(err, res){
 			expect(res.status).toBe(405);
 			expect(res.text).toEqual('Method not allowed');
